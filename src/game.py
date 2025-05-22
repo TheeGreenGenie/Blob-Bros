cb5e08f86aaa9ff0bb2dae0aac9e157999d38847
TheeGreenGenie <solomonshasanmi@gmail.com>
2025-05-22 16:55:37 -0700

    init for game class

---
+++ src/game.py
@@ -0,0 +1,40 @@
+#Main game class
+
+import arcade
+from . import settings
+
+class PlatformGame(arcade.Window):
+    #Main game class managing window, game loop, & game state
+
+    def __init__(self):
+        #initialize game window
+        super().__init__(
+            settings.SCREEN_WIDTH,
+            settings.SCRREEN_HEIGHT,
+            settings.SCREEN_TITLE
+        )
+
+        arcade.set_background_color(settings.SKY_BLUE)
+
+        self.current_state = settings.GAME_STATES["PLAYING"]
+
+        self.player_list = None
+        self.wall_list = None
+        self.coin_list = None
+        self.enemy_list  = None
+
+        self.player_sprite = None
+
+        self.physics_engine = None # For Collisions & Movement
+
+        #Camera for scrolling
+        self.camera = None
+        self.gui_camera = None
+
+        self.score = 0
+        self.lives = settings.PLAYER_LIVES
+        self.level_complete = False
+
+        self.frame_count = 0
+
+        self.show_debug = settings.DEBUG_MODE
