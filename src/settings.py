9d8fc8ce7b6a60ab0ab43615a0a765cd7b32a3d8
TheeGreenGenie <solomonshasanmi@gmail.com>
2025-05-22 16:29:04 -0700

    more constants

--- src/settings.py
+++ src/settings.py
@@ -32,3 +32,46 @@ JUMP_KEY = "SPACE"
 LEFT_KEY = "LEFT"
 RIGHT_KEY = "RIGHT"
 PAUSE_KEY = "P"
+
+ASSETS_PATH = "Mario_Case (Not Started)\MarioGame\assets"
+SPRITES_PATH = f"{ASSETS_PATH}/sprites"
+SOUNDS_PATH = f"{ASSETS_PATH}/sounds"
+LEVELS_PATH = "Mario_Case (Not Started)\MarioGame\levels"
+
+PLAYER_SPRITES = {
+    'small': f"{SPRITES_PATH}/player/mario_small.png",
+    'big': f"{SPRITES_PATH}/player/mario_big.png",
+    'fire': f"{SPRITES_PATH}/player/mario_fire.png"
+}
+
+ENEMY_SPRITES = {
+    'goomba': f"{SPRITES_PATH}/enemies/goomba.png",
+    'koopa': f"{SPRITES_PATH}/enemies/koopa.png"
+}
+
+ITEM_SPRITES = {
+    'coin': f"{SPRITES_PATH}/items/coin.png",
+    'mushroom': f"{SPRITES_PATH}/items/mushroom.png",
+    'fire_flower': f"{SPRITES_PATH}/items/fire_flower.png"
+}
+
+BLOCK_SPRITES = {
+    'ground': f"{SPRITES_PATH}/blocks/ground.png",
+    'brick': f"{SPRITES_PATH}/blocks/brick.png",
+    'question': f"{SPRITES_PATH}/blocks/question.png",
+    'pipe': f"{SPRITES_PATH}/blocks/pipe.png"
+}
+
+SOUND_FILES = {
+    'jump': f"{SOUNDS_PATH}/sfx/jump.wav",
+    'coin': f"{SOUNDS_PATH}/sfx/coin.wav",
+    'powerup': f"{SOUNDS_PATH}/sfx/powerup.wav",
+    'enemy_stomp': f"{SOUNDS_PATH}/sfx/enemy_stomp.wav",
+    'game_over': f"{SOUNDS_PATH}/sfx/game_over.wav",
+    'level_complete': f"{SOUNDS_PATH}/sfx/level_complete.wav"
+}
+
+MUSIC_FILES = {
+    'overworld': f"{SOUNDS_PATH}/music/overworld.ogg",
+    'underground': f"{SOUNDS_PATH}/music/underground.ogg"
+}
